ed626607e3df7773c137aec973e86410bc1c5398
Dmitry Galyuk <dmitry@galyuk.com>
2023-06-28 22:00:43 +0400

    dark mode, more mindmap code

--- js/render.js
+++ js/render.js
@@ -4,11 +4,18 @@ export default class Renderer {
     constructor() {
         this.linkMaxWidth = 50;
 
+        let dark = false;
+
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            dark = true;
+        }
+
         mermaid.initialize({
             startOnLoad: true,
             htmlLabels: true,
             // securityLevel: "loose",
             maxTextSize: 9000000,
+            darkMode: dark,
             flowchart: {
                 useMaxWidth: 0,
                 padding: 10,
@@ -17,6 +24,7 @@ export default class Renderer {
             },
             mindmap: {
                 useMaxWidth: true
+                // padding: 20
             }
         });
     }
@@ -65,21 +73,118 @@ export default class Renderer {
 
     renderMermaidMindmap(targetElement, root) {
         let result = ["mindmap\n"];
-        function traverse(node, level) {
-            if (!level) level = 1;
-            renderNode(node, level);
+        let styleSheet;
+        function traverse(node, callback, level=1) {
+            callback(node, level);
             for (let child of node.children) {
-                traverse(child, level+1);
+                traverse(child, callback, level+1);
             }
         }
         function renderNode(n, level) {
             result.push(`${'\t'.repeat(level)}${level==1?"root":""}(${n.title}\n`);
             result.push(`${'\t'.repeat(level)}${n.name}\n`);
             result.push(`${'\t'.repeat(level)}${n.overallstructuretotal.toFixed(0)} / ${n.grouptotal.toFixed(0)} / ${n.personalvolume})\n`);
+            result.push(`${'\t'.repeat(level)}:::n${n.id}\n`);
+        }
+        function renderStyles(n, level) {
+            styleSheet.insertRule(`.n${n.id} {fill: hsl(${n.hue}, ${n.saturation}%, ${n.light}%);}`);
+            styleSheet.insertRule(`.n${n.id} tspan {fill: yellow;}`)
+        }
+        function mapValue(value, fromRange, toRange) {
+            let fromMin = fromRange[0];
+            let fromMax = fromRange[1];
+            let toMin = toRange[0];
+            let toMax = toRange[1];
+        
+            let fromSpan = fromMax - fromMin;
+            if (fromSpan == 0) return 0;
+
+            let toSpan = toMax - toMin;
+        
+            let valueScaled = (value - fromMin) / fromSpan;
+        
+            return toMin + (valueScaled * toSpan);
+        }
+
+        function assignColors(node, odd=true) {
+            if(!node.parent) {
+                node.hueRange = [0, 360];
+            } 
+            // assign hueRangeSpan to each child having structure 
+            for (let c of node.children) {
+                if (c.children && c.children.length>0) {
+                    c.hueSpan = mapValue(c.overallstructuretotal, [0, node.overallstructuretotal], node.hueRange);
+                }
+                else {
+                    c.hueSpan = 0;
+                }
+            }
+
+            let hueRangeEnd = 0;
+            for (let i=node.children?.length % 2; i<node.children?.length; i+=2) {
+                node.children[i].hueRange = [hueRangeEnd, hueRangeEnd+node.children[i].hueSpan];
+                hueRangeEnd += node.children[i].hueSpan;
+            }
+            for (let i = node.children?.length-1; i>=0; i-=2) {
+                node.children[i].hueRange = [hueRangeEnd, hueRangeEnd+node.children[i].hueSpan];
+                hueRangeEnd += node.children[i].hueSpan;
+            }
+
+            if (!node.parent) {
+                node.hue = 0;
+                node.saturation = 0;
+                node.light = 0;
+            }
+            else if(node.titleObject?.order >= Member.directorOrder) {
+                node.hue = Math.ceil( (node.hueRange[1] - node.hueRange[0])/2 );
+                node.saturation = mapValue(
+                    node.titleObject.order, 
+                    [Member.directorOrder, Math.max(...Member.order.map(o=>o.order))],
+                    [50, 100]);
+                node.light = 50;
+            }
+            else {
+                node.hue = node.parent.hue;
+                node.saturation = 20;
+                node.light = 50;
+            }
+
+            for (let c of node.children) {
+                assignColors(c);
+            }
+
+
+        }
+
+        function assignHashLinks(target, root) {
+            let nodes = document.querySelectorAll(".mindmap-node");
+            for (let node of nodes) {
+                let classes = node.classList;
+                for (let cssClass of classes) {
+                    if( cssClass.startsWith("n")) {
+                        let id = cssClass.substring(1);
+                        let textNode = node.querySelector("text");
+                        let textContents = textNode.innerHTML;
+                        let foreign = document.createElement("foreignObject");
+                        foreign.innerHTML = `<a href="#${id}">${textContents}</a>`;
+                        textNode.innerHTML = "";
+                        textNode.appendChild(foreign);
+                    }
+                }
+            }
         }
         
-        traverse(root);
-        mermaid.render('mindmap', result.join('')).then(this._getMermaidRenderCallback(targetElement));
+        traverse(root, renderNode);
+        assignColors(root, [0, 360]);
+        mermaid.render('mindmap', result.join('')).then(this._getMermaidRenderCallback(targetElement)).then(()=>{
+        styleSheet = targetElement.querySelector("style").sheet;
+            for(let i=0;i<styleSheet.cssRules.length;i++) {if(styleSheet.cssRules[i].selectorText.includes(".edge")) {styleSheet.deleteRule(i)}}
+            for(let i=0;i<styleSheet.cssRules.length;i++) {if(styleSheet.cssRules[i].selectorText.includes(".section")) {styleSheet.deleteRule(i)}}
+
+            traverse(root, renderStyles);
+        })//.then(() => assignHashLinks(targetElement, root));
+
+        
     }
 
     _getMermaidRenderCallback(templateNode) {
