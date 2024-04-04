63f921c7c0274fa7cf046990fa35e27d3db85cc1
Dmitry Galyuk <dmitry@galyuk.com>
2024-04-04 02:22:29 +0200

    hovered parents highlighting

--- src/js/treeview.js
+++ src/js/treeview.js
@@ -4,34 +4,52 @@ export default class Treeview {
     }
 
     render(dataRoot) {
-        let htmlContainer = document.createElement("DIV");
+        let htmlContainer = document.createElement("UL");
         this.renderNode(htmlContainer, dataRoot);
-        this.targetElement.innerHTML = htmlContainer.innerHTML;
-        
+        this.targetElement.innerHTML = htmlContainer.outerHTML;
+
         let lis = this.targetElement.querySelectorAll("LI");
         lis.forEach(
-            li => li.addEventListener("click", (e) => {
-                li.classList.toggle("wrapped");
-                e.stopPropagation();
-            })
+            li => {
+                li.addEventListener("click", (e) => {
+                    li.classList.toggle("wrapped");
+                    e.stopPropagation();
+                });
+                li.addEventListener('mouseover', () => {
+                    // Select all the parent elements of the hovered li
+                    let parentLi = li.parentElement.closest('li');
+
+                    // Apply styles to each parent li element
+                    while (parentLi !== null) {
+                        parentLi.classList.add('hoveredParent');
+                        parentLi = parentLi.parentElement.closest('li');
+                    }
+                });
+
+                li.addEventListener('mouseout', () => {
+                    // Remove styles from all li elements
+                    document.querySelectorAll('.hoveredParent').forEach(el => {
+                        el.classList.remove('hoveredParent');
+                    });
+                });
+            }
         );
-        
+
     }
 
     renderNode(htmlRoot, dataNode) {
-        let ul = document.createElement("UL");
-        htmlRoot.appendChild(ul);
         let li = document.createElement("LI");
         li.textContent = dataNode.name;
-        
-        ul.appendChild(li);
-        if (dataNode.children) {
+
+        htmlRoot.appendChild(li);
+        if (dataNode.children && dataNode.children.length > 0) {
             let nestedUl = document.createElement("UL");
             li.appendChild(nestedUl);
+
+            for (let child of dataNode.children) {
+                this.renderNode(nestedUl, child)
+            }
         }
 
-        for (let child of dataNode.children) {
-            this.renderNode(li, child)
-        }
     }
 }
