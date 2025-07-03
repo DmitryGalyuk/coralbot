4c71ef415e7c2168aa498e64d907047ae5204340
Dmitry Galyuk <dmitry@galyuk.com>
2025-07-03 17:12:41 +0300

    director Pending total calculation

--- azurefunc/report2table.py
+++ azurefunc/report2table.py
@@ -26,6 +26,7 @@ def report_to_table(report: bytes):
 
     df_recast = df_noNA.convert_dtypes(convert_floating=True, convert_integer=True)
     df_recast["Personal"].astype(float)
+    df_recast["Pending"].astype(float)
     df_recast["Id"].astype(int)
 
     df_gen = df_recast
@@ -43,21 +44,40 @@ def report_to_table(report: bytes):
     members = {}
     parents = {}
 
-    def calculate_overallstructure_total(id):
+    def calculate_structure_totals(id):
         member = members[id]
         member["Overall"] += member["Personal"] or 0
+        member["StructurePending"] += member["Pending"] or 0
         if id in parents:
             children = [c for c in members.values() if c["ParentId"] == member["Id"]]
             for c in children:
-                member["Overall"] += calculate_overallstructure_total(c["Id"])
-        return member["Overall"]
+                calculate_structure_totals(c["Id"])
+                members[c["ParentId"]]["Overall"] += c["Overall"] or 0
+                members[c["ParentId"]]["StructurePending"] += c["StructurePending"] or 0
+
 
     df_gen["Overall"] = 0
+    df_gen["StructurePending"] = 0
     d = df_gen.to_dict(orient="index")
     members = {r["Id"] : r for r in d.values()}
     parents = {r["ParentId"] : r["Id"] for r in d.values()}
-    calculate_overallstructure_total(parents[-1])
+    calculate_structure_totals(parents[-1])
     memberOverall = {x["Id"]: x["Overall"] for x in members.values()}
+    memberStructurePending = {x["Id"]: x["StructurePending"] for x in members.values()}
     df_gen["Overall"] = df_gen["Id"].map(memberOverall)
+    df_gen["StructurePending"] = df_gen["Id"].map(memberStructurePending)
 
     return df_gen
+
+def test():
+    from pathlib import Path
+    test_file_path = Path(__file__).parent.parent / "data" / "2025-07-03.xlsx"
+    report = open(test_file_path.resolve(), "rb").read()
+    df = report_to_table(report)
+    options.display.float_format = "{:,.2f}".format
+    print(df)
+    df.to_csv("result.csv", index=False, encoding="utf-8-sig")
+
+
+if __name__ == "__main__":
+    test()
